777539c7f41ac947742d469714ebc9b3a403decb
Kacper Marczewski <kacper.marczewski@gmail.com>
2019-08-28 21:23:30 +0200

    logging improved for ANN

--- src/main/java/com/km/engine/ANNEngine.java
+++ src/main/java/com/km/engine/ANNEngine.java
@@ -1,5 +1,6 @@
 package com.km.engine;
 
+import com.km.Logger;
 import com.km.game.GameController;
 import com.km.game.Move;
 import com.km.nn.NetUtil;
@@ -19,19 +20,24 @@ public class ANNEngine implements MoveEngine {
         double score;
         double bestScore = Double.MIN_VALUE;
         Move best = null;
+        Logger.debug(String.format("algo\tnumber of available moves = [%d]", moves.size()));
         for (Move m : moves) {
             score = scoreMove(m);
+            Logger.debug(String.format("algo\tmove = [%d, %d] score = [%f]", m.getI(), m.getJ(), score));
             if (bestScore < score) {
                 bestScore = score;
                 best = m;
             }
         }
+        Logger.debug(String.format("algo\tbest = [%d, %d] score = [%f]", best.getI(), best.getJ(), bestScore));
         return best;
     }
 
     private double scoreMove(Move m) {
         GameController copy = controller.copy();
+        Logger.setDebugOff();
         copy.updateBoard(m);
+        Logger.setDebugOn();
         return NetUtil.process(copy.getGameBoard().toDBString());
     }
 
