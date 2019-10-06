37f73af53ab970752d76d0857d61c3c9c0e33eb5
Kacper Marczewski <kacper.marczewski@gmail.com>
2019-10-06 11:49:26 +0200

    tuning

--- src/main/java/com/km/game/GameRunner.java
+++ src/main/java/com/km/game/GameRunner.java
@@ -49,20 +49,10 @@ public class GameRunner {
     }
 
     private void runTrainingCycle(int i, int trainCycleLen) {
-        switch (i % 4) {
-            case 0:
-                runWars(EngineType.MCT, EngineType.RANDOM, trainCycleLen);
-                break;
-            case 1:
-                runWars(EngineType.RANDOM, EngineType.MCT, trainCycleLen);
-                break;
-            case 2:
-                runWars(EngineType.MCT, EngineType.ANN, trainCycleLen);
-                break;
-            case 3:
-                runWars(EngineType.ANN, EngineType.MCT, trainCycleLen);
-                break;
-        }
+        if ((i % 2) == 0)
+            runWars(EngineType.MCT, EngineType.RANDOM, trainCycleLen);
+        else
+            runWars(EngineType.RANDOM, EngineType.MCT, trainCycleLen);
     }
 
     public void startWarGame(EngineType typeB, EngineType typeW, int count) {
