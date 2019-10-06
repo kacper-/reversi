c24c4241c1ab690908526fdfcdd22be8969e884e
Kacper Marczewski <kacper.marczewski@gmail.com>
2019-10-06 02:22:39 +0200

    tuning

--- src/main/java/com/km/nn/Layer.java
+++ src/main/java/com/km/nn/Layer.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 class Layer implements Serializable {
     private final static double WEIGHT_INIT_LIMIT = 0.05d;
-    private final static double LEARNING_FACTOR = 0.005d;
+    private final static double LEARNING_FACTOR = 0.004d;
     private double[][] weights;
     private double[][] weightDeltas;
     private double[] outputs;
