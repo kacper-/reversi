02d29047bdc97a1de38bc65c807adfc60150fc8b
Kacper Marczewski <kacper.marczewski@gmail.com>
2019-10-10 16:40:40 +0200

    bug fix

--- src/test/java/com/km/nn/NetTest.java
+++ src/test/java/com/km/nn/NetTest.java
@@ -71,7 +71,7 @@ public class NetTest {
 
     @Test
     public void teach() {
-        Net net = new NetUtil(NetUtil.NET_VERSION).createInstance();
+        Net net = new NetUtil(NetVersion.NET2).createInstance();
         int count = 5000 * test.length;
         int t;
         for (int i = 0; i < count; i++) {
