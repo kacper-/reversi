3233ad3f8567a2f2e2524fe54571872c5a007c25
Kacper Marczewski <kacper.marczewski@gmail.com>
2019-11-20 22:14:18 +0100

    tuning

--- src/main/java/com/km/nn/NetUtil.java
+++ src/main/java/com/km/nn/NetUtil.java
@@ -9,6 +9,7 @@ import com.km.repos.GameService;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class NetUtil {
     private static final double PRECISION = 0.1d;
@@ -96,7 +97,7 @@ public class NetUtil {
         List<Nodes> nodes = new ArrayList<>(GameService.getNodes());
         int count = nodes.size();
         for (int i = 0; i < count; i++) {
-            train(nodes.get(i));
+            train(nodes.get(ThreadLocalRandom.current().nextInt(count)));
         }
         int result = verify(nodes);
         Logger.info(String.format("net\ttraining accuracy : [%.2f] -> [%d %%] after [%d] iterations", PRECISION, result, trainCount));
